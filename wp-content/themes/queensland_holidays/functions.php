e51b15d008699632b514edd2992fafd369a9235f
vitthal <bulevitthal@gmail.com>
2017-06-13 17:30:37 +0530

    footer gallery home page

--- wp-content/themes/queensland_holidays/functions.php
+++ wp-content/themes/queensland_holidays/functions.php
@@ -271,3 +271,43 @@ function proprty_taxonomy() {
 
 }
 add_action( 'init', 'proprty_taxonomy', 0 );
+
+
+// Register Custom Taxonomy
+function property_location() {
+
+	$labels = array(
+		'name'                       => _x( 'Locations', 'Taxonomy General Name', 'text_domain' ),
+		'singular_name'              => _x( 'Location', 'Taxonomy Singular Name', 'text_domain' ),
+		'menu_name'                  => __( 'Location', 'text_domain' ),
+		'all_items'                  => __( 'All Locations', 'text_domain' ),
+		'parent_item'                => __( 'Parent Location', 'text_domain' ),
+		'parent_item_colon'          => __( 'Parent Location:', 'text_domain' ),
+		'new_item_name'              => __( 'New Location Name', 'text_domain' ),
+		'add_new_item'               => __( 'Add New Location', 'text_domain' ),
+		'edit_item'                  => __( 'Edit Location', 'text_domain' ),
+		'update_item'                => __( 'Update Location', 'text_domain' ),
+		'view_item'                  => __( 'View Location', 'text_domain' ),
+		'separate_items_with_commas' => __( 'Separate Locations with commas', 'text_domain' ),
+		'add_or_remove_items'        => __( 'Add or remove Locations', 'text_domain' ),
+		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+		'popular_items'              => __( 'Popular Locations', 'text_domain' ),
+		'search_items'               => __( 'Search Locations', 'text_domain' ),
+		'not_found'                  => __( 'Not Found', 'text_domain' ),
+		'no_terms'                   => __( 'No Locations', 'text_domain' ),
+		'items_list'                 => __( 'Locations list', 'text_domain' ),
+		'items_list_navigation'      => __( 'Locations list navigation', 'text_domain' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => true,
+		'show_in_nav_menus'          => true,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'property_location', array( 'property' ), $args );
+
+}
+add_action( 'init', 'property_location', 0 );
